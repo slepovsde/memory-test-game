a894aee33dbe4434b51d4e1b074f7da7181079dd
slepovsde <slepovsde@gmail.com>
2021-11-18 12:46:24 +0700

    memory game vue3

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="app">
+    <img class="logo" alt="Vue logo" src="./assets/logo.png">
+    
+    <h1 class="h1">MEMORY TEST</h1>
+    
+    <Board />
+  </div>
+</template>
+
+<script>
+import Board from './components/Board';
+export default {
+  name: 'App',
+  components: {
+    Board,
+  }
+}
+</script>
+
+<style scoped>
+  .app {
+    font-family: Helvetica, sans-serif;
+    text-align: center;
+    margin-top: 40px;
+  }
+  .logo {
+    width: 100px;
+  }
+</style>
---
+++ src/components/Board.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="board-wrapper">
+    <div class="board">
+      <BoardItem :game-status="gameStatus" v-for="field in fields" :field="field" :key="'item-' + field.id"
+        @selectField="selectField($event)"/>
+    </div>
+    
+    <p class="difficult">Сложность: <strong>{{ difficult }}</strong></p>
+    <p class="win" v-if="isWin">Поздравляем! Продолжаем играть!</p>
+    <p class="fail" v-if="isFail">Вы проиграли. Попробуйте еще раз!</p>
+  
+    <button class="btn" @click="start" :disabled="!canStartGame">Старт</button>
+  </div>
+</template>
+
+<script>
+import BoardItem from './BoardItem';
+import useGameInit from "@/components/composables/useGameInit";
+import useGameStart from "@/components/composables/useGameStart";
+import useGameProcess from "@/components/composables/useGameProcess";
+import { GAME_STATUS } from "@/constants";
+import { ref } from 'vue';
+export default {
+  name: 'Board',
+  props: {},
+  components: {
+    BoardItem,
+  },
+  setup() {
+    const number = 25;
+    const gameStatus = ref(GAME_STATUS.NONE);
+    
+    const { difficult, fields, init } = useGameInit(number);
+    
+    const { start, canStartGame } = useGameStart(init, fields, difficult, number, gameStatus);
+    const { selectField, isWin, isFail } = useGameProcess(fields, gameStatus, difficult, start);
+    
+    return {
+      number,
+      difficult,
+      fields,
+      init,
+      start,
+      gameStatus,
+      canStartGame,
+      selectField,
+      isWin,
+      isFail
+    }
+  },
+}
+</script>
+
+<style scoped>
+  .board-wrapper {
+    margin-bottom: 100px;
+  }
+  
+  .board {
+    width: 300px;
+    background: #eee;
+    margin: 0 auto;
+  }
+  
+  .btn {
+    background: #794eddcc;
+    color: white;
+    border: none;
+    border-radius: 10px;
+    padding: 10px 30px;
+    margin: 10px 0;
+    cursor: pointer;
+    outline: none;
+  }
+  
+  button:hover {
+    background: #3d5be0;
+  }
+  
+  button:disabled {
+    opacity: .5;
+  }
+  
+  .win {
+    color: #7660d8;
+  }
+  
+  .fail {
+    color: #67007c75;
+  }
+</style>
